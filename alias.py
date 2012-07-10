3c727e1f2e93a55ac2cfee640329a76f65798467
lucifer <fortran95@126.com>
2012-07-10 15:21:15 +0800

    Init only.

---
+++ alias.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# config/addressbook.txt 的解析器
+# 用于输出：从一个应用的特定账户接受信息时，采用的证书
+#           以及通过特定应用指定接受账户时，采用的证书
+
+def get_certsubject(account,software):
+    pass
---
+++ letter.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# 提供一个类，用于解析或产生与上层应用交换的信息
+
+class letter(object):
+    
+    attributes = {}
+    items = ('SENDER','VIA','TAG','RECEIVER','ATTRIBUTES')
+
+    def __init__(self):
+        pass
+    def read(self,filename):
+        try:
+            
+            # 接收者和应用名称（这将决定证书信息，但本类不管）
+            # 应用标识（不管）
+            # 发送者（和应用名称）
+            
+            f = open(filename,'r').readlines()
+
+            separator = f.index('\n')
+
+            headers = f[0:separator]
+            body  = "".join(f[separator+1:]).strip()
+
+            self.attributes = {}
+            for l in headers:
+                l = l.strip()
+                prefix = l[0:l.index(' ')].strip().upper()
+                if prefix in self.items:
+                    value  = l[l.index(' ') + 1:].strip()
+                    self.attributes[prefix] = value
+
+            # 读取 ATTRIBUTES 给出的消息编码设置，解读消息
+            if self.attributes.has_key('ATTRIBUTES'):
+                attrs = self.attributes['ATTRIBUTES'].lower().split(';')
+                if 'base64' in attrs:
+                    body = body.decode('base64')
+            
+            self.body = body
+
+        except Exception,e:
+            print e
+            raise Exception("Unable to read this letter.")
+    def write(self,filename):
+        try:
+            
+            body = ''
+
+            # 根据body和attributes的设置，写入文件
+            for attr in self.attributes:
+                value = self.attributes[attr]
+                attr  = attr.upper()
+                if attr in self.items:
+                    body += '%16s %s\n' % (attr,value)
+            body += '\n'
+            
+            # 根据attributes做出处理
+            t = self.body[:]
+            if self.attributes.has_key('ATTRIBUTES'):
+                attrs = self.attributes['ATTRIBUTES'].lower().split(';')
+                if 'base64' in attrs:
+                    t = t.encode('base64')
+            
+            body += t
+
+            open(filename,'w+').write(body)                    
+
+        except Exception,e:
+            print e
+            raise Exception("Failed writing a letter.")
+
+if __name__ == '__main__':
+    l = letter()
+    l.attributes = {'receiver':'orxszlyzr','sender':'rijndael','via':'babeltower','tag':'tag'}
+    l.body = 'Hello, world!'
+    l.write('outbox/sample')
