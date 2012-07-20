e08642c248ff9e284a395dd60eaea9853f1634bf
lucifer <fortran95@126.com>
2012-07-20 15:39:13 +0800

    Added util: splitjsons, for future use.

--- _util.py
+++ _util.py
@@ -1,6 +1,21 @@
 import uuid,time,random
 def uniqid():
     return str(uuid.uuid5(uuid.uuid4(),str(time.time())))
-
-if __name__ == '__main__':
-    print uniqid()
+def splitjsons(text):
+    ret = []
+    text = text.strip()
+    unbalance = 0
+    starts = '{[('
+    ends   = '}])'
+    i      = 0
+    while text != '':
+        if text[i] in starts:
+            unbalance += 1
+        elif text[i] in ends:
+            unbalance -= 1
+        i += 1
+        if unbalance == 0:
+            ret.append(text[0:i])
+            text = text[i:].strip()
+            i = 0
+    return ret
