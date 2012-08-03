15bfbe24f495159af36a515c212f4dcc7b8f7fce
lucifer <fortran95@126.com>
2012-08-03 09:01:26 +0800

    Now works. More details needed.

--- tool.signnew.py
+++ tool.signnew.py
@@ -7,7 +7,7 @@ from xi.certificate import certificate
 from gui.selector   import selector
 from gui.inputbox   import inputbox
 from gui.pinreader  import pinreader
-import logging,os,sys,ConfigParser
+import logging,os,sys,ConfigParser,json
 
 log = logging.getLogger('xi.tool.signnew')
 
@@ -42,9 +42,64 @@ else:
 
 if jobid == 2: # 导入证书签名
     signtxt = inputbox('请将签名文本粘贴（Ctrl+V）到下面：','导入签名',True)
-    print signtxt
+    try:
+        signparsed = json.loads(signtxt)
+        certified  = signparsed['Content']['Certified_ID']
+        issued     = signparsed['Content']['Issuer_ID']
+    except:
+        log.warning('Given signature cannot be parsed. Data may corrupted.')
+        print '证书格式不正确。'
+        exit()
+    # Find issuer(public) and holder(private), though issuer may not be verified yet.
+    issuer, holder = None, None
+
+    c = certificate()
+    for u in publiclist:
+        c_path = os.path.join(_util.BASEPATH,publiclist[u])
+        c.load_public_text(open(c_path,'r').read())
+
+        cid = c.get_id()
+        if cid == certified:
+            if u in privatelist:
+                def _pinreader(b=False,p1='',p2=''):
+                    msg = u'正在导入密钥。\n请您输入密码解密以下证书：\n [%s]' % c.subject
+                    return pinreader(b,message=msg)
+
+                holder = certificate()
+                try:
+                    holder_savepath = os.path.join(_util.BASEPATH,privatelist[u])
+                    holder_pubpath  = c_path
+                    holder.load_private_text(holder_savepath,_pinreader)
+                except:
+                    print "解密证书失败，密码错误或者用户取消。"
+                    
+                    log.exception('Failed loading signature. Private certificate cannot be loaded, bad passphrase or user cancelled.')
+
+                    exit()
+        elif cid == issued:
+            issuer = c
+
+    if holder != None:
+        # holder must be a private certificate
+        try:
+            holder.load_signature(signparsed)
+        except Exception,e:
+            log.exception('Given signature cannot even pass primarily check.')
+            print "证书数据错误。"
+            exit()
+
+        print "保存私有证书。您应该不需为此再次输入密钥..."
+
+        holder.save_private_text(holder_savepath,_pinreader)
+
+        print "重新产生公钥证书并写入..."
+
+        pubtext = holder.get_public_text()
+
+        open(holder_pubpath,'w+').write(pubtext)
+
 else:
-    if jobid == 0:
+    if jobid == 0:  # FIXME
         pass
     elif jobid == 1:
         pass
