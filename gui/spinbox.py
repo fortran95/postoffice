6c04d0377feebd013057c13e8c35fadbde7a6d6c
lucifer <fortran95@126.com>
2012-08-04 11:57:53 +0800

    Renamed to spinbox.py from integerinput.py. This can be used for non-integer choosing.

---
+++ gui/spinbox.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# 接受在一定范围内的一个整数
+from Tkinter import *
+ret = False
+
+def spinbox(prompt,vtitle='Xi'):
+    global ret
+    ret = False
+
+    root = Tk()
+    
+    lbl = Label(root,text=prompt,justify=LEFT)
+    lbl.grid(row=0,column=0,columnspan=2,padx=5,pady=5)
+
+    entry = Spinbox(root,values=v)
+    entry.grid(row=1,column=0,columnspan=2,sticky=N+S+E+W)
+
+    btnOK = Button(root,text='确定')
+    def okcommand(r=root,e=entry):
+        global ret
+        ret = e.get()
+        r.destroy()
+    btnOK['command'] = okcommand
+    btnOK.grid(row=2,column=0)
+
+    btnCancel = Button(root,text='取消')
+    def cancelcommand(r=root):
+        global ret
+        ret = False
+        r.destroy()
+    btnCancel['command'] = cancelcommand
+    btnCancel.grid(row=2,column=1)
+
+    root.title(title)
+    root.resizable(0,0)
+    center_window(root)
+    root.mainloop()
+
+    try:
+        root.quit()
+    except:
+        pass
+    return ret
+
+if __name__ == '__main__':
+    print spinbox('hello')
