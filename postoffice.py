8928a37cadec7bf0f0779832f55a016d6cec6e72
lucifer <fortran95@126.com>
2012-08-23 21:37:44 +0800

    cli interface begin programming.

---
+++ postoffice.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Command-line management tool.
+
+from cmd import *
+import os
+
+class postoffice_shell(Cmd):
+    prompt = 'Xi.Postoffice > '
+
+    def do_EOF(self,line) : exit()
+    def do_exit(self,line): self.do_EOF(line)
+    def do_quit(self,line): self.do_EOF(line)
+
+    def help_EOF(self)    : print 'Sending an EOF(usually by pressing <CTRL+D>) or using command <quit>, <exit> will exit this shell.'
+    def help_help(self)   : print 'Use <help command_name> to show help on commands.'
+
+    def help_exit(self): self.help_EOF()
+    def help_quit(self): self.help_EOF()
+
+    def default(self,line): print "Bad command: %s" % line
+    def emptyline(self): pass
+
+if __name__ == '__main__':
+    os.system('clear')
+
+    sh = postoffice_shell()
+
+    intro = '\n    Xi.Postoffice::Shell\n\n #IMPORTANT# Examine these checksums to check system integrity:\n  * Root Certificates:\n'
+
+    sh.cmdloop(intro)
