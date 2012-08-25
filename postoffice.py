a3df15aa26115d46ee8a3834ac7b44c67e463b7f
lucifer <fortran95@126.com>
2012-08-25 16:45:08 +0800

    Include selfcheck directly, simplified interface in CLI

--- postoffice.py
+++ postoffice.py
@@ -6,29 +6,38 @@ from cmd import *
 from _util import colorshell
 import os,sys,argparse
 
+import tool_selfcheck
+
 BASEPATH = os.path.realpath(os.path.dirname(sys.argv[0]))
 
 class postoffice_shell(Cmd):
     prompt = 'Xi.Postoffice > '
 
     def cmdloop(self, intro=None):
+        selfcheck = self.do_selfcheck('silent')
+
         os.system('clear')
 
+        emptyline = '*' + 78 * ' ' + '*'
         print '*' * 80
         print '*' + colorshell(' Welcome to Postoffice ',1,0).center(88,' ') + '*'
         print '* This is user interface to top-secret system Xi\'s frontend.' + 19 * ' ' + '*'
-        print '*' + 78 * ' ' + '*'
+        print emptyline
         print '* %-74s *' % 'To get help: %s' % colorshell('help',1,0)
         print '* %-66s *' % 'To exit: %s or %s or %s' % (colorshell('exit',1,0),colorshell('quit',1,0),colorshell('<Ctrl+D>',1,0))
-        print colorshell(' Attention! ',31).center(91,'*')
-        print '* %-56s *' % '%s checking system integrity with: %s' % (colorshell('ALWAYS remember',33,1),colorshell('selfcheck',1,0))
+        print emptyline
+        print '* %-59s *' % 'System Checksum: %s' % colorshell(tool_selfcheck.friendly_display(selfcheck['total']).strip(),31,1)
+        print '* %-56s *' % '%s checking system integrity with: %s' % (colorshell('ALWAYS remember',31,1),colorshell('selfcheck',1,0))
         print '*' * 80
-        self.do_selfcheck('')
         Cmd.cmdloop(self, intro)
 
     def do_issue(self,line):
         print line
-    def do_selfcheck(self,line): self._run_python('tool.selfcheck.py')
+    def do_selfcheck(self,line):
+        if line.lower().startswith('silent'):
+            return tool_selfcheck.do(True)
+        else:
+            return tool_selfcheck.do(False)
 
     def do_EOF(self,line) : os.system('clear'); self.do_selfcheck(''); exit()
     def do_exit(self,line): self.do_EOF(line)
---
+++ tool_selfcheck.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import os,sys,zlib
+from xi.hashes import Hash
+from _util import colorshell
+
+BASEPATH = os.path.realpath(os.path.dirname(sys.argv[0]))
+def ls(parameters):
+    global BASEPATH
+    r2 = []
+    for path, suffix in parameters:
+        lst = os.listdir(path)
+        r1 = []
+        if suffix != None:
+            for each in lst:
+                if each.endswith(suffix): r1.append(each)
+        else:
+            r1 = lst
+        r1.sort()
+        for each in r1:
+            r2.append(os.path.join(BASEPATH,path,each))
+    return r2
+
+def sumfiles(filelist):
+    global BASEPATH
+    hashpart = []
+
+    for each in filelist:
+#        hashpart.append(Hash('whirlpool',open(each,'r').read()).hexdigest())  #
+        hashpart.append(Hash('sha1',open(each,'r').read()).digest())
+
+    return Hash('sha224',''.join(hashpart)).hexdigest()
+    
+def sumhashes(hashlist):
+    hashlist.sort()
+    return Hash('sha224',''.join(hashlist)).hexdigest()
+
+def friendly_display(h,centerw=80):
+    h = h.upper()
+    ret = ''
+    while h != '':
+        ret += h[0:4] + ' '
+        h = h[4:]
+    return ret.strip().center(centerw)
+
+def do(silent=True):
+    items = [
+        ('Root public certificates', [('xi/user/rootcerts',None)]),
+        ('Config Files'            , [('config/','alias.cfg')]),
+        ('Kernel Programs'         , [('xi/','.py'),('xi/ciphers','.py'),('xi/hashes','.py'),('.','.py'),('gui/','.py')]),
+    ]
+    result = {}
+    print "Self-checking, this may take a few minutes."
+    for itemname, listp in items:
+        print "Calculating checksums: %s" % itemname
+        result[itemname] = sumfiles(ls(listp))
+
+    hashsum = sumhashes(result.values())
+    total = str(hex(zlib.crc32(hashsum) & 0xFFFFFFFF))[2:] + str(hex(zlib.adler32(hashsum) & 0xFFFFFFFF))[2:]
+
+    if not silent:
+        print colorshell("Compare following checksums with your paper records.",1,0)
+        print "A mismatch is a sign of modification to the relative part."
+        print "If you're not sure why mismatch, be cautious using the entire system."
+        print "If you're confident with system security and integrity, update paper records.\n"
+
+        print 'Total checksum (for quick examination): %s' % colorshell(friendly_display(total).strip(),32,1)
+        i = 1
+        for itemname in result:
+            print "[%d] %s:\n%s" % (i, itemname, colorshell(friendly_display(result[itemname]),32,1))
+            i += 1
+    result = {'total':total,'parts':result}
+    return result
+
+if __name__ == '__main__':
+    print do()
